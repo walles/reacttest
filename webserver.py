b3590b30c57718ae69c65ed1d9e0ac7d2bef640e
Johan Walles <johan.walles@gmail.com>
2018-01-20 15:15:41 +0100

    Open a web browser when calling "run.sh"

--- webserver.py
+++ webserver.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 import BaseHTTPServer
+import webbrowser
 
 # "0" gets us a random port: https://stackoverflow.com/a/442074/473672
 server_address = ('localhost', 0)
 
 # FIXME: This thing should serve files from webui/build
 httpd = BaseHTTPServer.HTTPServer(server_address, BaseHTTPServer.BaseHTTPRequestHandler)
-print("Serving on http://localhost:" + str(httpd.server_port))
+url = "http://localhost:" + str(httpd.server_port)
+print("Serving on " + url)
 
+# FIXME: There's a race condition here; we should really start serving before we
+# open the browser. How do we even know when the web server has started serving?
+webbrowser.open(url)
 httpd.serve_forever()
